5339d1eb67dffb65957a974a2c13486cbcfd8736
Zachary Murray <89foxlx@gmail.com>
2020-02-20 00:10:06 -0600

    idk. followed the instructions as best as i could. not working at this point

--- src/index.js
+++ src/index.js
@@ -5,25 +5,45 @@ import axios from "axios";
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 
+import { useLocalStorage } from './hooks/useLocalStorage';
+import { useDarkMode } from './hooks/useDarkMode';
+
 import "./styles.scss";
 
 const App = () => {
-  const [coinData, setCoinData] = useState([]);
+    const [coinData, setCoinData] = useState([]);
+    //const test = useLocalStorage('dark-mode', false);
+    const [testDarkMode, setTestDarkMode] = useDarkMode();
+    let [dMode, setDMode] = useState('App');
 
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
-      )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
-  }, []);
-  return (
-    <div className="App">
-      <Navbar />
-      <Charts coinData={coinData} />
-    </div>
-  );
+    useEffect(() => {
+        axios
+        .get(
+            "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+        )
+        .then(res => setCoinData(res.data))
+        .catch(err => console.log(err));
+    }, []);
+
+    useEffect(() => {
+        // console.log(test);
+        console.log(testDarkMode);
+        //console.log(testSetDarkMode);
+        
+        if(testDarkMode) {
+            setDMode('dark-mode');
+        } else {
+            setDMode('App');
+            //console.log('test');
+        }
+    }, [testDarkMode]);
+
+    return (
+        <div className={dMode}>
+        <Navbar />
+        <Charts coinData={coinData} />
+        </div>
+    );
 };
 
 const rootElement = document.getElementById("root");
